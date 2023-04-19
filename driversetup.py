e67f2ebecf82a622ab81625e0ca7d8563449115a
Muhammad Ganjar Imanudin <ganjar_ibrahimovic@rocketmail.com>
2023-04-19 12:44:31 +0700

    driversetup

---
+++ driversetup.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+
+def driversetup():
+    options = webdriver.ChromeOptions()
+    #run Selenium in headless mode
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    #overcome limited resource problems
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("lang=en")
+    #open Browser in maximized mode
+    options.add_argument("start-maximized")
+    #disable infobars
+    options.add_argument("disable-infobars")
+    #disable extension
+    options.add_argument("--disable-extensions")
+    options.add_argument("--incognito")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+
+    driver = webdriver.Chrome(options=options)
+
+    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined});")
+
+    return driver
